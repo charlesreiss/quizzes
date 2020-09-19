849b2fc7066d86008409f865af539b73cd826c3c
cr4bd <cr4bd@virginia.edu>
2020-09-19 16:35:16 -0400

    extra dumpanswers support

--- dumpanswers.php
+++ dumpanswers.php
@@ -6,14 +6,28 @@ if (php_sapi_name() != "cli") {
 }
 
 $qid = $argv[1];
+if (count($argv) > 2) {
+    $sid = $argv[2];
+} else {
+    $sid = FALSE;
+}
 
 $answers = array();
+$qobj = qparse($qid);
 
-foreach (glob("log/$qid/*.log") as $j=>$logname) {
-    $sid = pathinfo($logname, PATHINFO_FILENAME);
-    $qobj = qparse($qid);
-    $sobj = aparse($qobj, $sid);
-    $answers[$sid] = $sobj;
+if ($sid === FALSE) {
+    foreach (glob("log/$qid/*.log") as $j=>$logname) {
+        $sid = pathinfo($logname, PATHINFO_FILENAME);
+        $sobj = aparse($qobj, $sid);
+        $answers[$sid] = $sobj;
+    }
+} else {
+    if (count($argv) > 3) {
+        $time_cutoff = strtotime($argv[3]);
+    } else {
+        $time_cutoff = FALSE;
+    }
+    $answers = aparse($qobj, $sid, $time_cutoff);
 }
 echo json_encode($answers, JSON_PRETTY_PRINT);
 
