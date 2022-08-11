65207e85ee5a0f31263145ad1dc0770d537ab597
Charles Reiss <creiss@virginia.edu>
2022-08-10 20:43:28 -0400

    CLI dumping scripts for other scripts

---
+++ dump_answers.php
@@ -0,0 +1,37 @@
+<?php
+require_once "tools.php";
+
+if (php_sapi_name() != "cli") {
+    die("CLI only");
+}
+
+$qid = $argv[1];
+if (count($argv) > 2) {
+    $sid = $argv[2];
+} else {
+    $sid = FALSE;
+}
+
+$answers = array();
+$qobj = qparse($qid);
+
+if ($sid === FALSE) {
+    foreach (glob("log/$qid/*.log") as $j=>$logname) {
+        $sid = pathinfo($logname, PATHINFO_FILENAME);
+        $sobj = aparse($qobj, $sid);
+        grade($qobj, $sobj);
+        $answers[$sid] = $sobj;
+    }
+} else {
+    if (count($argv) > 3) {
+        $time_cutoff = strtotime($argv[3]);
+    } else {
+        $time_cutoff = FALSE;
+    }
+    $answers = aparse($qobj, $sid, $time_cutoff);
+    grade($qobj, $answers);
+}
+echo json_encode($answers, JSON_PRETTY_PRINT);
+
+?>
+
